aedda241c5fed492d427b30f2726f26a7df6250c
kaihokua <21cu0207@jec.ac.jp>
2023-07-13 11:19:02 +0900

    GunPlayer 作成

---
+++ Source/NEO/PlayerGun.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerBase.h"
+#include "PlayerGun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEO_API APlayerGun : public APlayerBase
+{
+	GENERATED_BODY()
+	
+};
