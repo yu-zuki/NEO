24fe3524451b7690ef0a5d8ad24ba17deef9ede5
kaihokua <21cu0207@jec.ac.jp>
2023-07-19 13:22:40 +0900

    gun 追加
    commit ec63bfef5ff3d804ce05d6f96c45cf8a1bafe685
    Author: kaihokua <21cu0207@jec.ac.jp>
    Date:   Wed Jul 19 13:22:24 2023 +0900
    
        オーバーライド
    
    commit a3a202de60fafc7507b4bf61db78e5007487eef4
    Author: soma0000 <137361433+soma0000@users.noreply.github.com>
    Date:   Mon Jul 17 00:34:06 2023 +0900
    
        おそらく治った
    
    commit 2c124ef4ffcb361f83ab109db3274f7307dc9435
    Author: soma0000 <137361433+soma0000@users.noreply.github.com>
    Date:   Sun Jul 16 23:39:09 2023 +0900
    
        PlayerGun更新
    
    commit bd44beb709238c37a6cf299dc4767171c6ce0b21
    Author: soma0000 <137361433+soma0000@users.noreply.github.com>
    Date:   Sun Jul 16 23:38:34 2023 +0900
    
        アニメーションアセット

---
+++ Source/NEO/PlayerSystem/PlayerGun.cpp
@@ -0,0 +1,132 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerGun.h"
+#include "PlayerBase.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "Engine/StreamableManager.h"
+#include "Engine/World.h"
+#include "Engine/AssetManager.h"
+#include "Async/Async.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+APlayerGun::APlayerGun()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// プレイヤーの設定
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	// キャラクターコンポーネント取得
+	CharacterMovementComp = GetCharacterMovement();
+	CharacterMovementComp->MaxWalkSpeed = 500.f;
+
+	// プレイヤーのデータセット
+	SetupPlayerData();
+
+}
+
+void APlayerGun::BeginPlay()
+{
+
+}
+
+void APlayerGun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APlayerGun::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+
+}
+
+// プレイヤーのデータを初期化
+void APlayerGun::SetupPlayerData()
+{
+	Super::SetupPlayerData();
+
+	// 武器のメッシュのパス
+	TCHAR* WeaponAssetPath = TEXT("/Game/0122/Player/Weapon/Weapons/Blade/Swords/Blade_BlackKnight/SK_Blade_BlackKnight");
+
+	// セット
+	SetupWeaponMesh(WeaponAssetPath, "WeaponMesh");
+
+	// アニメーションセットアップ
+	TCHAR* AnimationAssetPaths[2];
+
+	// アニメーションアセットのパス
+	AnimationAssetPaths[0] = TEXT("/Game/0207/GunAnimation/montage/GunCombo_1.GunCombo_1");
+	//AnimationAssetPaths[0] = TEXT("/Game/0122/Player/Animation/Montage/Combo/SwordCombo");
+
+	AnimationAssetPaths[1] = TEXT("/Game/0122/Player/Animation/Montage/Combo/SwordCombo2");
+
+	SetupAnimationAsset(AnimationAssetPaths);
+
+	//WeaponCollision = SetupCollisionComponent(WeaponCollision);
+}
+
+// 攻撃
+void APlayerGun::Attack(int AttackNum)
+{
+	if (!IsAttacking)
+	{
+		// 攻撃中フラグオン
+		IsAttacking = true;
+	}
+	else
+	{
+		// コンボ可能な時,継続
+		if (CanCombo)
+		{
+			// ラストアタックまでコンボ継続
+			if (ComboStartSectionNames[ComboIndex] != ComboStartSectionNames.Last())
+			{
+				++ComboIndex;
+			}
+		}
+	}
+
+	// 攻撃のアニメーション再生
+	PlayAnimation(ComboAnimMontages[AttackNum], ComboStartSectionNames[ComboIndex]);
+
+}
+
+// 攻撃
+void APlayerGun::Combo1()
+{
+	// コントロール可能か
+	if (!IsControl) { return; }
+
+	// 攻撃
+	Attack(0);
+
+}
+
+
+// 攻撃
+void APlayerGun::Combo2()
+{
+	// コントロール可能か
+	if (!IsControl) { return; }
+
+	// 攻撃
+	Attack(1);
+
+}
+
+
+
---
+++ Source/NEO/PlayerSystem/PlayerGun.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerBase.h"
+#include "PlayerGun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEO_API APlayerGun : public APlayerBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerGun();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//武器の当たり判定
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SwordCollision", meta = (AllowPrivateAccess = "true"))
+		class UBoxComponent* WeaponCollision;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// プレイヤーのデータを初期化
+	virtual void SetupPlayerData() override;
+
+
+	// 攻撃
+	virtual void Attack(int AttackNum = 0) override;
+
+
+	// 一つ目のコンボ
+	virtual void Combo1() override;
+
+	// 二つ目のコンボ
+	virtual void Combo2()override;
+
+};
