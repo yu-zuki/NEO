a9b022796c2d968434fe0bacab5d67e6d7e301c3
kyupi45 <93104313+kyupi45@users.noreply.github.com>
2023-07-24 09:50:12 +0900

    fixBug
    commit f2e795a9f9b742b2ebbf5b0dac8f9c56800aeb7e
    Author: kyupi45 <93104313+kyupi45@users.noreply.github.com>
    Date:   Mon Jul 24 09:49:42 2023 +0900
    
        fixbug
    
    commit 4a189b597bb6d7556296ac99335e9a3f750acc2e
    Author: kyupi45 <93104313+kyupi45@users.noreply.github.com>
    Date:   Mon Jul 24 09:49:21 2023 +0900
    
        fixbug

---
+++ Source/NEO/Enemy/DeathTrigger.cpp
@@ -0,0 +1,45 @@
+#include "DeathTrigger.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/Character.h"
+
+// Sets default values
+ADeathTrigger::ADeathTrigger()
+{
+    // Set this actor to call Tick() every frame
+    PrimaryActorTick.bCanEverTick = true;
+
+    // Create and position a sphere component
+    TriggerSphere = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
+    RootComponent = TriggerSphere;
+
+    // Set the sphere's collision profile
+    TriggerSphere->SetCollisionProfileName(TEXT("Trigger"));
+
+    // Event is triggered when sphere begins to overlap another actor
+    TriggerSphere->OnComponentBeginOverlap.AddDynamic(this, &ADeathTrigger::OnOverlapBegin);
+}
+
+// Called when the game starts or when spawned
+void ADeathTrigger::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+// Called every frame
+void ADeathTrigger::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void ADeathTrigger::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (OtherActor && OtherActor != this && OtherActor->ActorHasTag("Enemy"))
+    {
+        // Assuming the other actor is a character
+        ACharacter* OtherCharacter = Cast<ACharacter>(OtherActor);
+        if (OtherCharacter && OtherCharacter->GetMesh() && MyAnimMontage)
+        {
+            OtherCharacter->PlayAnimMontage(MyAnimMontage);
+        }
+    }
+}
---
+++ Source/NEO/Enemy/DeathTrigger.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DeathTrigger.generated.h"
+
+UCLASS()
+class NEO_API ADeathTrigger : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    ADeathTrigger();
+
+    // Sphere collision component
+    UPROPERTY(VisibleAnywhere, Category = "Trigger")
+        class USphereComponent* TriggerSphere;
+
+    // Reference to an AnimMontage
+    UPROPERTY(EditAnywhere, Category = "Animation")
+        class UAnimMontage* MyAnimMontage;
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Overlap begin function
+    UFUNCTION()
+        void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
