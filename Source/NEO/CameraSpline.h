d848f9bcf386cc083a7fe3a3f121c9b932e1725a
kaihokua <138839513+kaihokua@users.noreply.github.com>
2023-08-18 05:37:30 +0900

    0818

---
+++ Source/NEO/CameraSpline.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CameraSpline.generated.h"
+
+class USplineComponent;
+
+UCLASS()
+class NEO_API ACameraSpline : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACameraSpline();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	//ƒXƒvƒ‰ƒCƒ“
+		/** Camera boom positioning the camera behind the character */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Spline)
+		class USplineComponent* SplineComp;
+
+
+};
