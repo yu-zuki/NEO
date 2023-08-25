38b5b78355c7acdaf6052029d12882e0fad529ff
kyupi45 <yundaman8810@gmail.com>
2023-08-25 14:02:15 +0900

    Squashed commit of the following:
    commit c3fdc81bb9c37df7093628bf803b8e1ae3901ab7
    Merge: 02069c68 b7df2805
    Author: YUZUKI <ai1072308083@gmail.com>
    Date:   Fri Aug 25 12:34:41 2023 +0900
    
        SplineCameraAdd
    
    commit b7df280547444dd4e2def7bfb0dc1abe7be6c4cd
    Author: YUZUKI <ai1072308083@gmail.com>
    Date:   Fri Aug 25 12:34:08 2023 +0900
    
        SplineCameraを追加
    
        SplineCameraを追加

---
+++ Source/NEO/GameSystem/SplineCamera.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SplineCamera.generated.h"
+
+UCLASS()
+class NEO_API ASplineCamera : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	// Sets default values for this actor's properties
+	ASplineCamera();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//Create CameraSpringArm 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+		class USpringArmComponent* CameraSpringArm;
+	//Create Camera
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+		class UCameraComponent* CameraComponent;
+
+	//Create Spline
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+		class USplineComponent* SplineComponent;
+
+public:
+	//Camera Speed
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+		float CameraSpeed;
+
+	//Camera MoveFlag
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+		bool bMoveFlag;
+
+	//CurrentDistance
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+		float CurrentDistance;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
