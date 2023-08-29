9002db87aac412ba26968f7702f97fccd2177b97
kyupi45 <yundaman8810@gmail.com>
2023-08-29 16:12:45 +0900

    でっかい柵破壊

---
+++ Source/NEO/BackGroundSystem/BigFence.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ObjectBase.h"
+#include "BigFence.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEO_API ABigFence : public AObjectBase
+{
+	GENERATED_BODY()
+public:
+	ABigFence();
+};
---
+++ Source/NEO/BackGroundSystem/GeoBigFEnce.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GeometryCollection/GeometryCollectionActor.h"
+#include "GeoBigFEnce.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEO_API AGeoBigFEnce : public AGeometryCollectionActor
+{
+	GENERATED_BODY()
+public:
+	AGeoBigFEnce();
+};
