32e1fc734d6d1fcbd2c533da4bd6034791f2bf95
soma0000 <21cu0122@jec.ac.jp>
2023-09-12 10:11:32 +0900

    Weapon仕様変更
    commit 9055f888b7e1461a76bad06804d1ee55a94d27e0
    Author: soma0000 <21cu0122@jec.ac.jp>
    Date:   Tue Sep 12 10:11:16 2023 +0900
    
        a

--- Source/NEO/Enemy/Lancer.cpp
+++ Source/NEO/Enemy/Lancer.cpp
@@ -43,10 +43,8 @@ void ALancer::BeginPlay()
 
         if (Weapon)
         {
-            Weapon->AttachToHand(this, "enemy_R_handSocket");
+            Weapon->AttachToHand(this, "enemy_R_handSocket",EOwnerType::OwnerType_Enemy);
         }
-
-        Weapon->SetOwnerType(EOwnerType::OwnerType_Enemy);
     }
 }
 
