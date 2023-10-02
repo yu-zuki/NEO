a23a4dc20ce83d47c6d9f6b1f095af5c12163e64
matubokkurin <03yuma31@gmail.com>
2023-10-03 00:53:21 +0900

    a

--- Source/NEO/EnemyAIController.cpp
+++ Source/NEO/EnemyAIController.cpp
@@ -3,3 +3,47 @@
 
 #include "EnemyAIController.h"
 
+
+AEnemyAIController::AEnemyAIController()
+{
+
+}
+
+AActor* AEnemyAIController::EnemySpawn(ASpawnPoint* spawnPoint)
+{
+	//NULL Check
+	if (!spawnPoint) {
+		UE_LOG(LogTemp, Error, TEXT("SpawnPoint is not found"));
+		return nullptr;
+	}
+
+	//Transform‚ðŽæ“¾‚·‚é
+	FTransform spawnTransform = spawnPoint->GetTransform();
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	AActor* spawn_Actor = GetWorld()->SpawnActor<AActor>(spawnPoint->GetSpawnActorClass(), spawnPoint->GetActorLocation(), spawnPoint->GetActorRotation(), SpawnParams);
+
+
+	AOdaBase* Boss = Cast<AOdaBase>(spawn_Actor);
+	AEnamyBase* Enemy = Cast<AEnamyBase>(spawn_Actor);
+
+	if (Enemy) {
+		Enemy->SetActorTransform(spawnTransform);
+		Enemy->IsAreaEnemy = true;		//Flag Set 
+
+		return Enemy;
+	}
+	else if (Boss) {
+		Boss->SetActorTransform(spawnTransform);
+		Boss->IsAreaEnemy = true;		//Flag Set 
+
+
+		return Boss;
+	}
+
+	return nullptr;
+
+
+}
--- Source/NEO/EnemyAIController.h
+++ Source/NEO/EnemyAIController.h
@@ -4,16 +4,35 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "NEO/GameSystem/SpawnPoint.h"
+#include "OdaBase.h"
+#include "Enemy/EnamyBase.h"
+#include "Templates/SubclassOf.h"								//TSubclassOfを使うために必要なヘッダー
 #include "EnemyAIController.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class NEO_API AEnemyAIController : public AAIController
 {
 	GENERATED_BODY()
+public:
+		AEnemyAIController();
 	//やること
 	//敵の出現削除
 	//ボス、敵のマップ移動時処理
+
+	//関数
+	//敵の出現------------------------------------------------
+		AActor* EnemySpawn(ASpawnPoint* spawnPoint);
+
+
+
+	//変数
+	//敵の出現------------------------------------------------
+	UPROPERTY(EditAnywhere)
+	TSubclassOf< class AActor >EnemyInfo = nullptr;
+
+
 };
